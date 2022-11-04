a4e6f610983026b966d240b017f4f01fde6ecaf6
Wati-Dev <raphaelgrougnetca@gmail.com>
2022-11-04 13:50:33 -0400

    Ajout du contenu principal dans les fichiers

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Fonction qui s'execute au lancement de la page
+ */
+function initialisation(){
+
+}
+
+
+window.addEventListener("DOMContentLoaded", initialisation, false);
